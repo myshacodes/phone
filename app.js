91946a8286ae05d6045625a7663ea7ba552175d0
myshacodes <52944820+myshacodes@users.noreply.github.com>
2019-08-18 00:25:05 -0500

    Starting to mimic a real phone, eh?

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // IIFE
-// Immediately Invoked Function Expression
-//
 
 (function() {
+  var screen = document.getElementById("button");
+  var lockScreen = document.getElementById("sidebutton");
+  screen.addEventListener("click", unlock);
+  lockScreen.addEventListener("click", lock);
+
   function timer() {
     var clock = document.getElementById("clock");
 
@@ -10,20 +13,51 @@
     var hh = time.getHours();
     var mm = time.getMinutes();
 
-    var ss = time.getSeconds();
-    mm = checkTime(mm);
-    ss = checkTime(ss);
+    hh = addZeroToTime(hh);
+    mm = addZeroToTime(mm);
 
-    clock.innerHTML = hh + ":" + mm + ":" + ss;
+    clock.innerHTML = hh + ":" + mm;
+
     var t = setTimeout(timer, 1000);
   }
 
-  function checkTime(i) {
+  function addZeroToTime(i) {
     if (i < 10) {
-      i = "0" + i;
-    } // add zero in front of numbers < 10
-    return i;
+      return "0" + i;
+    } else {
+      return i;
+    }
   }
 
   timer();
+
+  function unlock() {
+    var clock = document.getElementById("clock");
+    var homeScreen = (document.getElementById("screen").style.background =
+      'url("https://source.unsplash.com/300x500/?ice-cream")');
+    clock.className = "clockunlock";
+    document.getElementById("apps").style.display = "flex";
+    appColor();
+  }
+  function lock() {
+    var clock = document.getElementById("clock");
+    var homeScreen = (document.getElementById("screen").style.background =
+      'url("https://source.unsplash.com/300x500/?galaxy")');
+    clock.className = "clocklock";
+    document.getElementById("apps").style.display = "none";
+  }
+  function appColor() {
+    var colors = [
+      "forestgreen",
+      "lightcoral",
+      "dodgerblue",
+      "gold",
+      "peachpuff"
+    ];
+    var apps = document.getElementById("apps");
+    for (let i = 0; i < apps.children.length; i++) {
+      let app = apps.children[i];
+      app.style.backgroundColor = colors[i % colors.length];
+    }
+  }
 })();
