bb8d7bd3c24f1c7dd0ffeab889fa3730ffe4e4d3
myshacodes <52944820+myshacodes@users.noreply.github.com>
2019-08-18 17:07:00 -0500

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // IIFE
 
 (function() {
+  var colors = [
+    "mediumseagreen",
+    "lightcoral",
+    "dodgerblue",
+    "gold",
+    "lightsalmon"
+  ];
   var screen = document.getElementById("button");
   var lockScreen = document.getElementById("sidebutton");
   screen.addEventListener("click", unlock);
@@ -34,7 +41,7 @@
   function unlock() {
     var clock = document.getElementById("clock");
     var homeScreen = (document.getElementById("screen").style.background =
-      'url("https://source.unsplash.com/300x500/?ice-cream")');
+      'url("https://source.unsplash.com/300x500/?flower")');
     clock.className = "clockunlock";
     document.getElementById("apps").style.display = "flex";
     appColor();
@@ -47,17 +54,41 @@
     document.getElementById("apps").style.display = "none";
   }
   function appColor() {
-    var colors = [
-      "forestgreen",
-      "lightcoral",
-      "dodgerblue",
-      "gold",
-      "peachpuff"
-    ];
     var apps = document.getElementById("apps");
     for (let i = 0; i < apps.children.length; i++) {
       let app = apps.children[i];
       app.style.backgroundColor = colors[i % colors.length];
+      app.addEventListener("click", appHandler);
     }
+  }
+
+  function appHandler() {
+    console.log(this.id);
+    let appName = this.id;
+
+    if (appName === "twitter") {
+      console.log("Welcome to Twitter");
+      console.log("Post a tweet!");
+    } else if (appName === "facebook") {
+      console.log("Welcome to Facebook");
+      console.log("Wanna make social friends?");
+    } else if (appName === "github") {
+      console.log("Welcome to Github");
+      console.log("Create a new repository or commit updates!");
+    } else if (appName === "email") {
+      console.log("Welcome to Email");
+      console.log("Someone sent you an email");
+    }
+
+    // switch()
+  }
+
+  // var ra = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  console.log(randomize(colors));
+
+  function randomize(arr = []) {
+    if (arr.length == 0) return null;
+    let random = arr[Math.floor(Math.random() * arr.length)];
+    return random;
   }
 })();
